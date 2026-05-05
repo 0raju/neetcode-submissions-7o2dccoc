2d5b183922b1a0705b0e876e8a9bbdbdbaa7d991
Mehedi Hasan Raju <75312209+0raju@users.noreply.github.com>
2026-05-05 01:23:28 -0500

    Add: find-peak-element - submission-6

---
+++ Data Structures & Algorithms/find-peak-element/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        # if len(nums) == 1:
+        #     return 0
+
+        # for i in range(len(nums)):
+        #     left = nums[i - 1] if i > 0 else float('-inf')
+        #     right = nums[i + 1] if i < len(nums) - 1 else float('-inf')
+
+        #     if nums[i] > left and nums[i] > right:
+        #         return i
+        
+        left, right = 0, len(nums)-1
+
+        while left < right:
+            mid = left + (right-left)//2
+            if nums[mid] < nums[mid+1]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left
+
+
+
